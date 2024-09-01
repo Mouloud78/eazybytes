c7ee0e680498d80b8790ebf379c887eb526f6b60
mouloud78 <mouloudouldali78@gmail.com>
2024-09-01 15:12:02 -0400

    account microservice

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,10 @@
+package com.eazybytes.accounts.dto;
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private int accountNumber;
+    private String accountType;
+    private String branchAddress;
+}
